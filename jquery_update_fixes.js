829338eed853065f2e66252e9c5f8096cbc9cf4a
Oskar Schöldström <public@oxy.fi>
2015-08-17 13:47:24 -0500

    eslint: make js pass

--- jquery_update_fixes.js
+++ jquery_update_fixes.js
@@ -1,4 +1,7 @@
-(function($) {
+/* eslint no-param-reassign: 0 */
+(function ($) {
+  'use strict';
+
   // Hack to fix admin_menu
   // @see https://drupal.org/node/1961178
   if (!$.browser) $.browser = { msie: false };
