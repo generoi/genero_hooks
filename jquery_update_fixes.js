dca0cd42500593ca23e3991a0e7081fbd91934ad
Oskar Schöldström <public@oxy.fi>
2015-08-22 14:25:21 -0500

    commerce_kickstarter: fix jquery.ui.spinner relying on old jquery functionality

--- jquery_update_fixes.js
+++ jquery_update_fixes.js
@@ -2,7 +2,7 @@
 (function ($) {
   'use strict';
 
-  // Hack to fix admin_menu
+  // Hack to fix admin_menu and jquery.ui.spinner
   // @see https://drupal.org/node/1961178
   if (!$.browser) $.browser = { msie: false };
 }(jQuery));
